71f212882375ad6d8baf8aa9c38fc6d99c3cf3d2
Vasyl Onopriienko <regnise@gmail.com>
2024-08-29 19:24:38 +0300

    add spread and destructuring examples

---
+++ src/basics/spread-and_destructuring.ts
@@ -0,0 +1,22 @@
+const userObj = {
+  firstName: "Vasyl",
+  age: 30,
+};
+const person = { ...userObj, lastName: "Onopriienko" }; // SPREAD OPERATOR
+
+const someArr = ["one", "two"];
+const extenedArray = ["three", ...someArr]; // SPREAD OPERATOR
+console.log(extenedArray);
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((acc, item) => {
+    return acc + item;
+  }, 0);
+};
+
+const addedNumbers = add(1, 2, 3, 3.3);
+console.log(addedNumbers);
+
+// ARRAY AND OBJECT DESTRUCTURING
+const [three, ...others] = extenedArray;
+const { age, firstName: name1 } = person;
